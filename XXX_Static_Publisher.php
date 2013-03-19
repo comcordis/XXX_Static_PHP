6670b98a749863d9b22ef246303d8803da594609
vinceMeens <v.w.a.meens@comcordis.com>
2013-03-19 05:11:04 +0100

    Various changes

--- XXX_Static_Publisher.php
+++ XXX_Static_Publisher.php
@@ -30,7 +30,8 @@ abstract class XXX_Static_Publisher
 					'gif',
 					'png',
 					'ico',
-					'swf'
+					'swf',
+					'woff'
 				),
 				
 				'denyExtensions' => array
@@ -225,7 +226,7 @@ abstract class XXX_Static_Publisher
 				}
 				else
 				{
-					trigger_error('Unable to write destination file: "' . $destinationFilePath . '".');
+					trigger_error('Unable to write destination file: "' . $destinationFilePath . '".', E_USER_ERROR);
 				}
 			}
 			else
@@ -235,7 +236,7 @@ abstract class XXX_Static_Publisher
 		}
 		else
 		{
-			trigger_error('Source file: "' . $sourceFilePath . '" doesn\'t exist or isn\'t available.');
+			trigger_error('Source file: "' . $sourceFilePath . '" doesn\'t exist or isn\'t available.', E_USER_ERROR);
 		}
 		
 		return $result;
@@ -310,7 +311,7 @@ abstract class XXX_Static_Publisher
 		}
 		else
 		{
-			trigger_error('Source directory: "' . $sourceDirectoryPath . '" doesn\'t exist or isn\'t available.');	
+			trigger_error('Source directory: "' . $sourceDirectoryPath . '" doesn\'t exist or isn\'t available.', E_USER_ERROR);	
 		}		
 		
 		if ($disablePublishProfile)
@@ -366,6 +367,224 @@ abstract class XXX_Static_Publisher
 			{	
 				$result = true;
 			}
+		
+		return $result;
+	}
+	
+	public static function publishI18n ($sourcePath = '', $destinationPath = '')
+	{
+		global $XXX_I18n_Translations, $XXX_I18n_Localizations;
+		
+		$i18nPaths = self::getI18nPaths($sourcePath, $destinationPath);
+		
+		// translations
+		
+			foreach ($i18nPaths['translations'] as $translation)
+			{
+				$before = $XXX_I18n_Translations[$translation['translation']];
+				
+				$XXX_I18n_Translations[$translation['translation']] = array();
+				
+				include $translation['sourcePath'];
+				
+				$tempTranslations = $XXX_I18n_Translations[$translation['translation']];
+				
+				$content = '
+				XXX_DOM_Ready.addEventListener(function ()
+				{
+					if (!XXX_I18n_Translations.' . $translation['translation'] . ')
+					{
+						XXX_I18n_Translations.' . $translation['translation'] . ' = {};
+					}
+					
+					XXX_I18n_Translations.' . $translation['translation'] . ' = XXX_Array.merge(XXX_I18n_Translations.' . $translation['translation'] . ', ' . XXX_String_JSON::encode($tempTranslations) . ');
+				});';
+				
+				$XXX_I18n_Translations[$translation['translation']] = $before;
+				
+				XXX_FileSystem_Local::writeFileContent($translation['destinationPath'], $content);	
+			}
+		
+		// localizations
+		
+			foreach ($i18nPaths['localizations'] as $localization)
+			{
+				$before = $XXX_I18n_Localizations[$localization['localization']];
+				
+				$XXX_I18n_Localizations[$localization['localization']] = array();
+				
+				include $localization['sourcePath'];
+				
+				$tempLocalizations = $XXX_I18n_Localizations[$localization['localization']];
+				
+				$content = '
+				XXX_DOM_Ready.addEventListener(function ()
+				{
+					if (!XXX_I18n_Localizations.' . $localization['localization'] . ')
+					{
+						XXX_I18n_Localizations.' . $localization['localization'] . ' = {};
+					}
+					
+					XXX_I18n_Localizations.' . $localization['localization'] . ' = XXX_Array.merge(XXX_I18n_Localizations.' . $localization['localization'] . ', ' . XXX_String_JSON::encode($tempLocalizations) . ');
+				});';
+				
+				$XXX_I18n_Localizations[$localization['localization']] = $before;
+				
+				XXX_FileSystem_Local::writeFileContent($localization['destinationPath'], $content);	
+			}
+	}
+	
+	public static function getI18nPaths ($sourcePath = '', $destinationPath = '')
+	{
+		$result = array		
+		(
+			'translations' => array(),
+			'localizations' => array()
+		);
+		
+		$sourceI18nPath = XXX_Path_Local::extendPath($sourcePath, 'i18n');
+		$destinationI18nPath = XXX_Path_Local::extendPath($destinationPath, 'i18n');
+		
+		if (XXX_FileSystem_Local::doesDirectoryExist($sourceI18nPath))
+		{
+			// translations
+			
+				$sourceTranslationsPath = XXX_Path_Local::extendPath($sourceI18nPath, 'translations');
+				$destinationTranslationsPath = XXX_Path_Local::extendPath($destinationI18nPath, 'translations');
+				
+				if (XXX_FileSystem_Local::doesDirectoryExist($sourceTranslationsPath))
+				{
+					$directoryContent = XXX_FileSystem_Local::getDirectoryContent($sourceTranslationsPath, false);
+				
+					if ($directoryContent !== false)
+					{
+						if (XXX_Array::getFirstLevelItemTotal($directoryContent['directories']))
+						{
+							foreach ($directoryContent['directories'] as $directory)
+							{
+								$translation = $directory['directory'];
+																
+								$sourceTranslationPath = XXX_Path_Local::extendPath($sourceTranslationsPath, array($translation, 'translations.' . $translation . '.php'));
+								$destinationTranslationPath = XXX_Path_Local::extendPath($destinationTranslationsPath, array($translation, 'translations.' . $translation . '.js'));
+								
+								if (XXX_FileSystem_Local::doesFileExist($sourceTranslationPath))
+								{
+									$result['translations'][] = array
+									(
+										'translation' => $translation,
+										'sourcePath' => $sourceTranslationPath,
+										'destinationPath' => $destinationTranslationPath
+									);
+								}
+							}
+						}
+					}
+				}
+			
+			// localizations
+				
+				$sourceLocalizationsPath = XXX_Path_Local::extendPath($sourceI18nPath, 'localizations');
+				$destinationLocalizationsPath = XXX_Path_Local::extendPath($destinationI18nPath, 'localizations');
+				
+				if (XXX_FileSystem_Local::doesDirectoryExist($sourceLocalizationsPath))
+				{
+					$directoryContent = XXX_FileSystem_Local::getDirectoryContent($sourceLocalizationsPath, false);
+				
+					if ($directoryContent !== false)
+					{
+						if (XXX_Array::getFirstLevelItemTotal($directoryContent['directories']))
+						{
+							foreach ($directoryContent['directories'] as $directory)
+							{
+								$localization = $directory['directory'];
+								
+								$sourceLocalizationPath = XXX_Path_Local::extendPath($sourceLocalizationsPath, array($localization, 'localizations.' . $localization . '.php'));
+								$destinationLocalizationPath = XXX_Path_Local::extendPath($destinationLocalizationsPath, array($localization, 'localizations.' . $localization . '.js'));
+								
+								if (XXX_FileSystem_Local::doesFileExist($sourceLocalizationPath))
+								{
+									$result['localizations'][] = array
+									(
+										'localization' => $localization,
+										'sourcePath' => $sourceLocalizationPath,
+										'destinationPath' => $destinationLocalizationPath
+									);
+								}
+							}
+						}
+					}
+				}
+		}
+		
+		$sourceModulesPath = XXX_Path_Local::extendPath($sourcePath, 'modules');
+		$destinationModulesPath = XXX_Path_Local::extendPath($destinationPath, 'modules');
+		
+		if (XXX_FileSystem_Local::doesDirectoryExist($sourceModulesPath))
+		{			
+			$directoryContent = XXX_FileSystem_Local::getDirectoryContent($sourceModulesPath, false);
+			
+			if ($directoryContent !== false)
+			{
+				if (XXX_Array::getFirstLevelItemTotal($directoryContent['directories']))
+				{
+					foreach ($directoryContent['directories'] as $directory)
+					{
+						$tempResult = self::getI18nPaths(XXX_Path_Local::extendPath($sourceModulesPath, array($directory['directory'])), XXX_Path_Local::extendPath($destinationModulesPath, array($directory['directory'])));
+						
+						if ($tempResult)
+						{
+							foreach ($tempResult['translations'] as $translation)
+							{
+								$result['translations'][] = $translation;
+							}
+							
+							foreach ($tempResult['localizations'] as $localization)
+							{
+								$result['localizations'][] = $localization;
+							}
+						}
+					}
+				}
+			}
+		}
+				
+		if (XXX_Array::getFirstLevelItemTotal($result['translations']) == 0 && XXX_Array::getFirstLevelItemTotal($result['localizations']) == 0)
+		{
+			$result = false;
+		}		
+		
+		return $result;
+	}
+	
+	public static function publishJSVariableToProject ($project = '', $key = '', $value = '', $domReady = true, $resultFile = '', $publishProfile = '')
+	{
+		$content = 'var ' . $key . ' = ' . XXX_String_JSON::encode($value) . ';' . XXX_OperatingSystem::$lineSeparator;
+		
+		if ($domReady)
+		{
+			$temp = '';
+			$temp .= XXX_String::$lineSeparator;
+			$temp .= 'XXX_DOM_Ready.addEventListener(function ()' . XXX_String::$lineSeparator;
+			$temp .= '{' . XXX_String::$lineSeparator;
+			$temp .= $content . XXX_String::$lineSeparator;
+			$temp .= '});' . XXX_String::$lineSeparator;
+			$temp .= XXX_String::$lineSeparator;
+			
+			$content = $temp;
+		}
+		
+		$temporaryFilePath = XXX_Path_Local::extendPath(XXX_OperatingSystem::$temporaryFilesPathPrefix, array($project . '_' . $resultFile));
+		
+		XXX_FileSystem_Local::writeFileContent($temporaryFilePath, $content);
+		
+		$item = array
+		(
+			'sourcePath' => $temporaryFilePath,
+			'destinationPath' => XXX_Path_Local::extendPath(XXX_Path_Local::$deploymentSourcePathPrefix, array('httpServerJail', 'static', $project, $resultFile)),
+			'publishProfile' => $publishProfile
+		);
+		
+		$result = self::publishItem($item);
 		
 		return $result;
 	}
